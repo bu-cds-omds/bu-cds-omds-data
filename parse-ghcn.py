d20bf26ef70b6c81ef6f4c9f5a95363cdfe5b01b
Jeffrey Considine <jconsidi@gmail.com>
2024-05-17 16:17:22 -0400

    + parsing bug fixes

--- parse-ghcn.py
+++ parse-ghcn.py
@@ -2,6 +2,7 @@
 
 # https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt
 
+import re
 import sys
 
 print("stations\tdate\telement\tvalue\tmflag\tqflag\tsflag")
@@ -12,7 +13,7 @@ for line in sys.stdin:
     month = int(line[15:17])
     element = line[17:21]
 
-    for (day_0, day_offset) in enumerate(range(23, len(line), 8)):
+    for (day_0, day_offset) in enumerate(range(21, len(line), 8)):
         day = day_0 + 1
         day_data = line[day_offset:day_offset+8]
         if len(day_data) < 8:
@@ -23,7 +24,12 @@ for line in sys.stdin:
         qflag = day_data[6]
         sflag = day_data[7]
 
+        value = value.strip()
         if value == "-9999": # and mflag == "  " and qflag == "  " and sflag == "  ":
+            continue
+
+        if not re.fullmatch('-?[0-9]+$', value):
+            print("SKIPPING", station, year, month, day, element, repr(value), file=sys.stderr)
             continue
 
         print(f"{station}\t{year:04d}-{month:02d}-{day:02d}\t{element}\t{value}\t{mflag}\t{qflag}\t{sflag}")
